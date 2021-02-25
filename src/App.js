e040895933ebae301cb93e2a7e487ea4c40ebec2
unknown <dxshakil2@gmail.com>
2021-02-25 09:53:40 -0800

    project set-up

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+ 
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
